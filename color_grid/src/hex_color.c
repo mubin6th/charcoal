f279f4c462ae24e63932db7911fe7a10f82d1eeb
mubin6th <mubin.muhammad.the.7th@gmail.com>
2025-10-04 18:28:57 +0600

    src/include/version.h: macros should be used as few as possible. that was a mistake in my part.

--- color_grid/src/hex_color.c
+++ color_grid/src/hex_color.c
@@ -35,7 +35,7 @@ optional_uint32_t hex_color_from_string(const char *str)
 
 inline void hex_color_split(uint32_t color, uint8_t *out)
 {
-    out[0] = color >> 24;
+    out[0] = (color >> 24);
     out[1] = (color >> 16) & 0xff;
-    out[2] = (color >> 8) & 0xff;
+    out[2] = (color >>  8) & 0xff;
 }
